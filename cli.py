cc199d7ca7396a1fb176b5f76d0a9faada76697f
joan <{ID}+{username}@users.noreply.github.com>
2022-01-09 13:38:59 +0100

    Add improvements

--- cli.py
+++ cli.py
@@ -1,13 +1,18 @@
 import requests 
 import time
+import sys 
 
-while True:
-    response = requests.get("http://127.0.0.1:5000/ping", headers={"x-secret-key":"2286872938"})
-    print(response.text)
-    while "pong" in response.text:
-        response = requests.get("http://127.0.0.1:5000/ping", headers={"x-secret-key":"2286872938"})
+argument_header = sys.argv[1]
+
+def sending_requests(argument_header):
+    while True:
+        response = requests.get("http://127.0.0.1:5000/ping", headers={"x-secret-key":argument_header})
         print(response.text)
-    else:
-        value = response.headers['Retry-After']
-        value = float(value)
-        time.sleep(value)
+        time.sleep(1)
+        if "error" in response.text:
+            value = response.headers['Retry-After']
+            value = float(value)
+            time.sleep(value)
+
+if __name__ == "__main__":
+    sending_requests(argument_header)
